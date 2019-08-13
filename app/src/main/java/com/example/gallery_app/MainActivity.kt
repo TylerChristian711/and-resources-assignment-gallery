3a1dd705181de27e3567674448821c48c0e1eb2e
TylerChristian711 <tylerc71197@gmail.com>
2019-08-13 02:23:12 -0400

    project half done

--- app/src/main/java/com/example/gallery_app/MainActivity.kt
+++ app/src/main/java/com/example/gallery_app/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.gallery_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       var images= arrayOf(R.drawable.abstract_abstract_expressionism_art_2505693,
+                                      R.drawable.adventure_automobile_classic_2533092,
+                                      R.drawable.afterglow_beach_cliff_2555285,
+                                      R.drawable.alley_architecture_buildings_2526517,
+                                      R.drawable.architectural_design_architecture_bridge_2540653,
+                                      R.drawable.beautiful_breathtaking_canada_day_2526105,
+                                      R.drawable.close_up_colorful_colors_2529148,
+                                      R.drawable.clouds_coconut_trees_daylight_2486168,
+                                      R.drawable.colorful_colourful_houses_2501965,
+                                      R.drawable.wallpaper_astronomy_astrophotography_2538107)
+
+        //
     }
 }
 //commit
